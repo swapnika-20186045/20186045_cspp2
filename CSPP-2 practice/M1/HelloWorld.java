db5c4c5c52b61f50aba8fe7d5113185323129153
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-27 18:40:09 +0530

    first program

---
+++ CSPP-2 practice/M1/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+*HelloWorld.
+*@author Swapnika.
+**/
+public final class HelloWorld {
+    /**
+    *empty constructor.
+    */
+    private HelloWorld() {
+        //I am not using this constructor
+    }
+    /**
+    *main function.
+    *@param args is parameter for this function.
+    **/
+    public static void main(final String[] args) {
+        System.out.println("Hello world!!");
+    }
+}
