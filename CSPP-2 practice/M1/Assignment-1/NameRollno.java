da3c01f703a722a20525fb8a941b8dca2b1cc39e
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-27 19:52:45 +0530

    name and rollnumber

---
+++ CSPP-2 practice/M1/Assignment-1/NameRollno.java
@@ -0,0 +1,20 @@
+/**
+*Print name and roll number.
+*@author Swapnika.
+**/
+public final class NameRollno {
+    /**
+    *empty constructor.
+    */
+    private NameRollno() {
+        //I am not using this constructor
+    }
+    /**
+    *main function.
+    *@param args is parameter for this function.
+    **/
+    public static void main(final String[] args) {
+        System.out.println("Name : Swapnika");
+        System.out.println("Roll Number : 20186045");
+    }
+}
