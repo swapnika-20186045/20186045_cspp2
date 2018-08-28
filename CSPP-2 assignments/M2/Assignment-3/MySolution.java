5ff015e9738901fc904b44f09038f4a30a422d92
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-28 14:56:44 +0530

    "check_style_score -> 4 of 5 passed. style score: 0/1 "

---
+++ CSPP-2 assignments/M2/Assignment-3/MySolution.java
@@ -0,0 +1,44 @@
+
+/**
+*Power of a number.
+*@author Swapnika
+**/
+import java.util.Scanner;
+/**
+*used to take inputs.
+**/
+public final class MySolution {
+    /**
+    *empty constructor.
+    **/
+    private MySolution() {
+    //I am not using this function
+    }
+    /**
+    *main function.
+    *@param args is parameter for this function.
+    **/
+    public static void main(final String[] args) {
+        int a, b;
+        int pow = 1;
+        Scanner s = new Scanner(System.in);
+        a = s.nextInt();
+        b = s.nextInt();
+        pow = power(a, b);
+        System.out.println(pow);
+    }
+    /**
+    *factorial function.
+    *@param a is parameter for this function.
+    *@param b is parameter for this function.
+    *@return int
+    **/
+    public static int power(final int a, final int b) {
+        if (b != 0) {
+            return (a * power(a, b - 1));
+        } else {
+            return 1;
+        }
+    }
+}
+
