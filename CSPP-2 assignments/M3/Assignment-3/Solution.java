1628a8e622dffefe72fcf941b4834cb4807d9d4a
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-29 12:30:20 +0530

    "check_style_score -> 1 of 5 passed. style score: 0/1 "

---
+++ CSPP-2 assignments/M3/Assignment-3/Solution.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+/*
+	Do not modify this main function.
+	*/
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner s=new Scanner(System.in);      
+        int n1 = s.nextInt();
+        int n2 = s.nextInt();
+        System.out.println(gcd(n1,n2));
+    }
+    /*
+	Need to write the gcd function and print the output.
+	*/
+    public static int gcd(final int n1, final int n2) {
+        int g = 0;
+        for(int i = 1; i <= n1 && i <= n2; ++i)
+        {
+            // Checks if i is factor of both integers
+            if(n1 % i == 0 && n2 % i == 0) {
+                return i;
+            }
+        }
+    return 1;
+}
+}
