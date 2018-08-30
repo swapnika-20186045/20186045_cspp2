c40cd2a19bfde4f7b202596fed38e20289701e37
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-30 15:51:29 +0530

    concatinate

---
+++ CSPP-2 practice/M4/Concatinate.java
@@ -0,0 +1,28 @@
+/**
+*return true if 6 appears as either the first or last element in the array.
+*@author Swapnika
+**/
+import java.util.Scanner;
+/**
+*used to take inputs.
+**/
+public final class Concatinate {
+    /**
+    *empty constructor.
+    **/
+    private Concatinate() {
+    //I am not using this function
+    }
+    /**
+    *main function.
+    *@param args is parameter for this function.
+    **/
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        String name = s.next();
+        /* Declaring array of n elements, the value
+         * of n is provided by the user
+         */
+        System.out.println("Hello " + name + "!");
+    }
+}
