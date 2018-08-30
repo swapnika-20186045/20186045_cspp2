ac3a2632003039bae6e2378ba519fcdda2b69480
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-30 14:55:51 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- CSPP-2 assignments/M4/Assignment-2/Solution.java
+++ CSPP-2 assignments/M4/Assignment-2/Solution.java
@@ -29,12 +29,12 @@ public final class Solution {
             }
         }
             
-        for (int i = 0; i < rows1; i++) {
-            for (int j = 0; j < cols1; j++) {
-                System.out.print(a[i][j] + " ");
-            }
-            System.out.println("");
-        }
+        // for (int i = 0; i < rows1; i++) {
+        //     for (int j = 0; j < cols1; j++) {
+        //         System.out.print(a[i][j] + " ");
+        //     }
+        //     System.out.println("");
+        // }
         rows2 = s.nextInt();
         cols2 = s.nextInt();
         int b[][] = new int[rows2][cols2];
@@ -43,12 +43,12 @@ public final class Solution {
                 b[i][j] = s.nextInt();
             }
         }
-        for (int i = 0; i < rows2; i++) {
-            for (int j = 0; j < cols2; j++) {
-                System.out.print(b[i][j] + " ");
-            }
-            System.out.println("");
-        }
+        // for (int i = 0; i < rows2; i++) {
+        //     for (int j = 0; j < cols2; j++) {
+        //         System.out.print(b[i][j] + " ");
+        //     }
+        //     System.out.println("");
+        // }
         int c[][] = new int[rows2][cols2];
         if (rows1 == rows2 && cols1 == cols2) {
             for (int i = 0; i < rows1; i++) {
