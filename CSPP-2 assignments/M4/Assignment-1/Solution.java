6cf9b4287ba72d12b34d3f9e6f6afcb7cc8da89c
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-30 12:47:24 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- CSPP-2 assignments/M4/Assignment-1/Solution.java
+++ CSPP-2 assignments/M4/Assignment-1/Solution.java
@@ -17,21 +17,20 @@ public final class Solution {
     *main function.
     *@param args is parameter for this function.
     **/
-	public static void main(String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		int n= sc.nextInt();
-		int [] a = new int[n];
-		for (int i = 0; i < n; i++) {
-			a[i] = sc.nextInt();
-		}
-		int max = a[0];
-		for (int i = 0; i < n; i++) {
-			if (max < a[i]) {
-				max = a[i];
-			}
-		}
-		System.out.println(max);
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int max = a[0];
+        for (int i = 0; i < n; i++) {
+            if (max < a[i]) {
+                max = a[i];
+            }
+        }
+        System.out.println(max);
 
-	}
+    }
 }
