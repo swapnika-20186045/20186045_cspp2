e13c410b212bd7ce295f986e967b537b1070a66c
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-30 15:00:29 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- CSPP-2 assignments/M4/Assignment-2/Solution.java
+++ CSPP-2 assignments/M4/Assignment-2/Solution.java
@@ -62,7 +62,8 @@ public final class Solution {
                 for (int j = 0; j < cols2; j++) {
                     System.out.print(c[i][j] + " ");
                 }
-            }
+                System.out.println("");
+                }
             } else {
                 System.out.println("not possible");
             }
