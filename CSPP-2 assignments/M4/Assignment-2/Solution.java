da178f3d1ad665be4c4c46974203a7e5c102808d
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-30 13:19:51 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ CSPP-2 assignments/M4/Assignment-2/Solution.java
@@ -0,0 +1,71 @@
+/**
+*Add two matrices.
+*@author Swapnika
+**/
+import java.util.Scanner;
+/**
+*used to take inputs.
+**/
+public final class Solution {
+    /**
+    *empty constructor.
+    **/
+    private Solution() {
+    //I am not using this function
+    }
+    /**
+    *main function.
+    *@param args is parameter for this function.
+    **/
+    public static void main(String[] args) {
+        int rows1, cols1, rows2, cols2;
+        Scanner s = new Scanner(System.in);
+        rows1 = s.nextInt();
+        cols1 = s.nextInt();
+        rows2 = s.nextInt();
+        cols2 = s.nextInt();
+        if (rows1 == rows2 && cols1 == cols2) {
+            int a[][] = new int[rows1][cols1];
+            int b[][] = new int[rows2][cols2];
+            int c[][] = new int[rows2][cols2];
+            for (int i = 0; i < rows1; i++) {
+                for (int j = 0; j < cols1; j++) {
+                    a[i][j] = s.nextInt();
+                }
+            }
+            
+            for (int i = 0; i < rows1; i++) {
+                for (int j = 0; j < cols1; j++) {
+                    System.out.print(a[i][j]+" ");
+                }
+                System.out.println("");
+            }
+            for (int i = 0; i < rows2; i++) {
+                for (int j = 0; j < cols2; j++) {
+                    b[i][j] = s.nextInt();
+                }
+            }
+            for (int i = 0; i < rows2; i++) {
+                for (int j = 0; j < cols2; j++) {
+                    System.out.print(b[i][j]+" ");
+                }
+                System.out.println("");
+            }
+            for (int i = 0; i < rows1; i++) {
+                for (int j = 0; j < cols2; j++) {
+                    for (int k = 0; k < cols1; k++) {
+                        c[i][j] = a[i][j] + b[i][j];
+                    }
+                }
+            }
+            for (int i = 0; i < rows1; i++) {
+                for (int j = 0; j < cols2; j++) {
+                    System.out.print(c[i][j]+" ");
+                }
+                System.out.println("");
+                }
+        } else {
+            System.out.println("Addition would not be possible");
+        }
+    }
+}
