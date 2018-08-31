cf687fbc3555c1230ce86394c82d7a521cd70520
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-31 14:26:44 +0530

    "check_style_score -> 0 of 1 passed. style score: 1/1 "

--- CSPP-2 assignments/M4/Assignment-3/Solution.java
+++ CSPP-2 assignments/M4/Assignment-3/Solution.java
@@ -21,6 +21,7 @@ public final class Solution {
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        sc.nextLine();
         for (int i = 0; i <= n; i++) {
             String s = sc.nextLine();
             long res = binaryToDecimal(s);
