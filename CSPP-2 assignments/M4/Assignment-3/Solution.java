290246413d490efce601d7b9fa42a6e63573df08
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-31 14:33:56 +0530

    "check_style_score -> 1 of 1 passed. style score: 1/1 "

--- CSPP-2 assignments/M4/Assignment-3/Solution.java
+++ CSPP-2 assignments/M4/Assignment-3/Solution.java
@@ -35,7 +35,7 @@ public final class Solution {
     **/
     public static int binaryToDecimal(final String s) {
         int sum = 0, power = 1;
-        for (int i = 0;i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '1') {
                 sum += Math.pow(2, s.length() - i - 1);
             }
