b0913af41d3de16ffea4bdc53cc078d4a8efc1c5
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-31 09:39:25 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

---
+++ CSPP-2 assignments/M4/Assignment-3/Solution.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+/**
+*returns the decimal value for the given binary string.
+*@author Swapnika.
+*/
+public class Solution {
+	/**
+    *empty constructor.
+    */
+    // private static final double PIVALUE = 22 / 7.0;
+    /**
+    *@variable PIVALUE
+    **/
+    private Solution() {
+        //I am not using this constructor
+    }
+    /**
+    *main function.
+    *@param args is parameter for this function.
+    **/
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 0;i <= n;i++) {
+			String s = sc.nextLine();
+			String res = binaryToDecimal(s);//Write binaryToDecimal function
+			System.out.println(res);
+		}
+	}
+	/**
+    *binary to decimal function.
+    <!-- *@param r is parameter for this function. -->
+    <!-- *@return double -->
+    **/
+    public static String binaryToDecimal(final String s) {
+        long sum = 0, power = 1;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            sum += ((int) (s.charAt(i)) - '0') * power;
+            power *= 2;
+        }
+        return sum + "";
+    }
+}
