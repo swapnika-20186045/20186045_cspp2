99f32c7bded594fe3f97d89cd455a878555f3a79
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-08-31 09:59:58 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- CSPP-2 assignments/M4/Assignment-4/Solution.java
+++ CSPP-2 assignments/M4/Assignment-4/Solution.java
@@ -17,22 +17,22 @@ public final class Solution {
     *main function.
     *@param args is parameter for this function.
     **/
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		String reverse = reverseString(s);	
-		System.out.println(reverse);
-	}
-	//Write reverseString function
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);
+        System.out.println(reverse);
+    }
+    //Write reverseString function
 
-	/**
-	 * { reverseString function }
-	 *
-	 * @param      s is parameter for this function.
-	 *
-	 * @return    string
-	 */
-	public static String reverseString(final String s) {
+    /**
+     * reverseString function.
+     *
+     * @param      s is parameter for this function.
+     *
+     * @return    string
+     */
+    public static String reverseString(final String s) {
         String rev = "";
         for (int i = s.length() - 1; i >= 0; i--) {
             rev += s.charAt(i);
