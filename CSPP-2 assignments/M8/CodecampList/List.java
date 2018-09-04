6398d857564321d8f4ad00021b4359ad205d1481
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-09-04 15:21:00 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

--- CSPP-2 assignments/M8/CodecampList/List.java
+++ CSPP-2 assignments/M8/CodecampList/List.java
@@ -310,6 +310,8 @@ public class List {
                 case "contains":
                 System.out.println(l.contains(Integer.parseInt(tokens[1])));
                 break;
+                default :
+                System.out.println();
             }
         }
     }
