8b362f7884cb3100457b6b95fac8581dfa0d9937
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-09-07 14:03:55 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- CSPP-2 assignments/M11/Assignment-1/List.java
+++ CSPP-2 assignments/M11/Assignment-1/List.java
@@ -415,8 +415,8 @@ public class List {
                     if (tokens.length == 2) {
                         String[] t1 = tokens[1].split(",");
                         int[] temp = new int[t1.length];
-                        for(int i = 0; i < temp.length; i++) {
-                            temp[i]=Integer.parseInt(t1[i]);
+                        for (int i = 0; i < temp.length; i++) {
+                            temp[i] = Integer.parseInt(t1[i]);
                         }
                         l.addAll(temp);
                     }
@@ -425,13 +425,14 @@ public class List {
                     if (tokens.length == 2) {
                         String[] t2 = tokens[1].split(",");
                         int[] a = new int[t2.length];
-                        for(int i = 0; i < t2.length; i++)
+                        for(int i = 0; i < t2.length; i++) {
                             a[i] = Integer.parseInt(t2[i]);
+                        }
                         l.removeAll(a);
                     }
                 break;
                 case "subList":
-                    if (tokens.length != 2) break;
+                    if (tokens.length != 2) {
                     String[] arrstring3 = tokens[1].split(",");
                     List object = l.subList(Integer.parseInt(arrstring3[0]),
                             Integer.parseInt(arrstring3[1]));
@@ -440,6 +441,7 @@ public class List {
 
                     }
                     break;
+                }
                 case "equals":
                     if (tokens.length == 2) {
                         String[] lt = tokens[1].split(",");
