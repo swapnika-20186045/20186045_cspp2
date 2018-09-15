d5b9312129b012374cc750616f61f54acc243e8d
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-09-15 12:51:33 +0530

    "check_style_score -> 7 of 13 passed. style score: 0/1 "

--- CSPP-2 assignments/M19 (exam 3)/Assignment-1/Solution.java
+++ CSPP-2 assignments/M19 (exam 3)/Assignment-1/Solution.java
@@ -11,23 +11,23 @@ class Quiz {
     /**
      * Question declaration as string.
      */
-    private String question;
+    String question;
     /**
      * choice declaration as String[].
      */
-    private String[] choice;
+    String[] choice;
     /**
      * correct variable declaration as string.
      */
-    private String correct;
+    String correct;
     /**
      * maxmarks variable declaration as string.
      */
-    private String maxMarks;
+    String maxMarks;
     /**
      * penality variable declaration as string.
      */
-    private String penality;
+    String penality;
     /**
      * Constructs the object.
      */
@@ -44,7 +44,7 @@ class Quiz {
      * @param      penalityy  The penalityy
      */
     Quiz(final String questionn, final String[] choicee, final String correctt,
-            final String maxMarkss, final String penalityy) {
+         final String maxMarkss, final String penalityy) {
         this.question = questionn;
         this.choice = choicee;
         this.correct = correctt;
@@ -63,13 +63,13 @@ public final class Solution {
     /**
      * using arraylist to use answers as list.
      */
-    private static ArrayList<Quiz> answers = new ArrayList<Quiz>();
+    private static ArrayList<String> answers = new ArrayList<String>();
     //private List<Quiz> quizes;
-     /**
-     * Constructs the object.
-     */
+    /**
+    * Constructs the object.
+    */
     private Solution() {
-    // quizes = new List<Quiz>();
+        // quizes = new List<Quiz>();
         // leave this blank
     }
     /**
@@ -78,37 +78,37 @@ public final class Solution {
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
-         // instantiate this Quiz
+        // instantiate this Quiz
         Quiz q = new Quiz();
-         // code to read the test cases input file
+        // code to read the test cases input file
         Scanner s = new Scanner(System.in);
         // check if there is one more line to process
         while (s.hasNext()) {
             // read the line
             String line = s.nextLine();
-             // split the line using space
+            // split the line using space
             String[] tokens = line.split(" ");
-              // based on the list operation invoke the corresponding method
+            // based on the list operation invoke the corresponding method
             switch (tokens[0]) {
-                case "LOAD_QUESTIONS":
+            case "LOAD_QUESTIONS":
                 System.out.println("|----------------|");
                 System.out.println("| Load Questions |");
                 System.out.println("|----------------|");
                 loadQuestions(s, q, Integer.parseInt(tokens[1]));
                 break;
-                case "START_QUIZ":
+            case "START_QUIZ":
                 System.out.println("|------------|");
                 System.out.println("| Start Quiz |");
                 System.out.println("|------------|");
                 startQuiz(s, q, Integer.parseInt(tokens[1]));
                 break;
-                case "SCORE_REPORT":
+            case "SCORE_REPORT":
                 System.out.println("|--------------|");
                 System.out.println("| Score Report |");
                 System.out.println("|--------------|");
                 displayScore(q);
                 break;
-                default:
+            default:
                 break;
             }
         }
@@ -121,7 +121,7 @@ public final class Solution {
      * @param      questionCount  The question count
      */
     public static void loadQuestions(final Scanner s, final Quiz quiz,
-                                        int questionCount) {
+                                     int questionCount) {
         // write your code here to read the questions from the console
         // tokenize the question line and create the question object
         // add the question objects to the quiz class
@@ -138,16 +138,16 @@ public final class Solution {
                     System.out.println("trick question  does not have enough answer choices");
                     return;
                 }
-                if (Integer.parseInt(tokens[3]) < 0){
+                if (Integer.parseInt(tokens[3]) < 0) {
                     System.out.println("Invalid max marks for question about sony");
                     return;
                 }
-                if (Integer.parseInt(tokens[4]) > 0){
+                if (Integer.parseInt(tokens[4]) > 0) {
                     System.out.println("Invalid penalty for question about sony");
                     return;
                 }
                 if (!tokens[2].equals("1") && !tokens[2].equals("2")
-                     && !tokens[2].equals("3") && !tokens[2].equals("4")) {
+                        && !tokens[2].equals("3") && !tokens[2].equals("4")) {
                     System.out.println("Error! Correct answer choice number is out of range for question text 1");
                     return;
                 }
@@ -171,11 +171,35 @@ public final class Solution {
      * @param      quiz         The quiz object
      * @param      answerCount  The answer count
      */
-    public static void startQuiz(final Scanner s, final Quiz quiz, final int answerCount) {
+    public static void startQuiz(final Scanner s, final Quiz quiz, int answerCount) {
         // write your code here to display the quiz questions
         // read the user responses from the console
         // store the user respones in the quiz object
         // System.out.println(quiz.getquestions());
+        for (int i = 0; i < quizes.size(); i++) {
+            System.out.println(quizes.get(i).question + quizes.get(i).maxMarks);
+            for (int j = 0 ; j < quizes.get(i).choice.length - 1; j++) {
+                System.out.print(quizes.get(i).choice[j]);
+            }
+            System.out.print(quizes.get(i).choice[quizes.get(i).choice.length - 1]);
+        }
+        while (answerCount > 0) {
+            //System.out.println(answerCount + " counttt");
+            String line = s.nextLine();
+            String[] token = line.split(" ");
+            //System.out.println(line + " lineeeee");
+            if (token[1].equals("a")) {
+                token[1] = "1";
+            } else if (token[1].equals("b") ) {
+                token[1] = "2";
+            } else if (token[1].equals("c")) {
+                token[1] = "3";
+            } else if (token[1].equals("d")) {
+                token[1] = "4";
+            }
+            answers.add(token[1]);
+            answerCount = answerCount - 1;
+        }
     }
 
     /**
