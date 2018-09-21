c7d5b6a7552e73b75ea2891bf9136c0232749f50
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-09-21 14:34:44 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- CSPP-2 assignments/M23/Assignment-1/Solution.java
+++ CSPP-2 assignments/M23/Assignment-1/Solution.java
@@ -3,117 +3,151 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.*;
-
+/**
+ * Class for document.
+ **/
 class Document {
-	String[] word;
-	HashMap<String, Integer> val;
-
+    String[] word;
+    HashMap<String, Integer> freq;
+    /**
+     * function to read input file.
+     **/
 	Document (String f) {
-		word = Formatread(f);
-		// System.out.println(Arrays.toString());
-
+		word = ReadFile(f);
 	}
-	public String[] Formatread(String f) {
-		String[] f1 = null;
-		String se = "";
+    /**
+     * Reads a file.
+     *
+     * @param      f     { parameter_description }
+     *
+     * @return     String[].
+     */
+	public String[] ReadFile(String f) {
+		String[] strfile = null;
+		String str = "";
 		try {
 			// File filename1 = new File("input000.txt");
-			Scanner s1 = new Scanner(new File(f));
-			while (s1.hasNext()) {
-				se += (s1.nextLine().replaceAll("[^a-zA-Z0-9_]+"," ").toLowerCase())+" ";
+			Scanner scan = new Scanner(new File(f));
+			while (scan.hasNext()) {
+                //converts to lowercase.
+				str += (scan.nextLine().toLowerCase());
 			}
-			f1 = se.split(" ");
+			strfile = str.split(" ");
 		} catch (FileNotFoundException e) {
 				e.printStackTrace();
-				// return null;
-			}
-		return f1;
-
+		}
+		return strfile;
 	}
+    /**
+     * to check frequency of a word.
+     *
+     * @return     HashMap<String, Integer>.
+     */
 	public HashMap<String, Integer> frequency() {
-		val = new HashMap<String, Integer>();
+		freq = new HashMap<String, Integer>();
 		for (int i = 0; i < word.length; i++) {
-			if (val.containsKey(word[i])) {
-				val.put(word[i], val.get(word[i]) + 1);
+			if (freq.containsKey(word[i])) {
+				freq.put(word[i], freq.get(word[i]) + 1);
 			} else {
-				val.put(word[i], 1);
+				freq.put(word[i], 1);
 			}
 		}
-		return val;
+		return freq;
 	}
-	// public String[] freqarrays() {
-	// 	Set name = val.keySet();
-	// 	System.out.println(name);
-	// 	return null;
-	// }
 }
+/**
+ * Class for distance.
+ */
 class Distance {
+    /**
+     * private declaration for a document.
+     */
     private Document doc1;
+    /**
+     * private declaration for a document.
+     */
     private Document doc2;
+    /**
+     * Constructs the object.
+     *
+     * @param      d1    The d 1
+     * @param      d2    The d 2
+     */
     Distance(Document d1, Document d2) {
-        doc1 = d1;
-        doc2 = d2;
-
+        this.doc1 = d1;
+        this.doc2 = d2;
     }
-    public double Euclidean(Document d) {
+    /**
+     * to find Euclidean norm.
+     *
+     * @param      doc   The document
+     *
+     * @return     double.
+     */
+    public double Euclidean(Document doc) {
     	HashMap<String,Integer> p = new HashMap<String,Integer>();
-    	p = d.frequency();
-        long sum = 0;
-        for(Map.Entry<String, Integer> m: p.entrySet()) {
-            // int val = (int)m.getValue();
-            sum += m.getValue()*m.getValue();
+    	p = doc.frequency();
+        int sum = 0;
+        for (String s : p.keySet()) {
+            sum += Math.pow(p.get(s), 2);
         }
-        double prod = Math.sqrt(sum);
-        return prod;
+        double norm = Math.sqrt(sum);
+        return norm;
     }
+    /**
+     * to find dotproduct.
+     *
+     * @return     double.
+     */
     public double DotProduct() {
     	HashMap<String, Integer> dict1 = doc1.frequency(); 	
     	HashMap<String, Integer> dict2 = doc2.frequency(); 	
-        String key;
         int sum = 0;
-        for (Map.Entry<String, Integer> m: dict1.entrySet()) {
-            key = m.getKey();
+        for ( String key : dict1.keySet()) {
             if (dict2.containsKey(key)) {
                 int val1 = dict1.get(key);
                 int val2 = dict2.get(key);
-                sum += (val1*val2);
+                sum += (val1 * val2);
             }
         }
         return sum;
     }
+    /**
+     * to find similarity.
+     *
+     * @return     double.
+     */
     public double similarity() {
         double a = Euclidean(doc1);
         double b = Euclidean(doc2);
-        double num = a*b;
-        double deno = DotProduct();
-        double result = deno/num;
-        result = result * 100;
+        double num = a * b;
+        double den = DotProduct();
+        double result = den / num;
+        result *= 100;
         return Math.round(result);
     }
 }
+/**
+ * Class for solution.
+ * main function.
+ */
 public class Solution {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
         String foldername = scan.nextLine();
-        // Inpfromfolder i = new Inpfromfolder();
         final File folder = new File(foldername);
-        // ArrayList<String> filenames= i.listFilesForFolder(folder);
         File[] allfiles = folder.listFiles();
-        // System.out.println(Arrays.toString(allfiles));
         String s = "\t";
-        for (File fil: allfiles) {
-            s += fil.getName() + "\t";
+        for (File file: allfiles) {
+            s += file.getName() + "\t";
         }
-        // s = s.trim();
-        // s = s.substring(0, s.length()-4);
         s += "\n";
         if (allfiles.length != 0) {
-            // System.out.println("Entered if");
-            for (File file1: allfiles) {
-                s += file1.getName() + "\t";
-                for (File file2: allfiles) {
-                    Document d1 = new Document(file1 + "");
-                    Document d2 = new Document(file2 + "");
+            for (int i = 0; i < allfiles.length; i++) {
+                s += allfiles[i].getName() + "\t";
+                for (int j = 0; j < allfiles.length; j++) {
+                    Document d1 = new Document(allfiles[i].getName());
+                    Document d2 = new Document(allfiles[j].getName());
                     Distance d = new Distance(d1, d2);
                     s += d.similarity() + "" + "\t";
                 }
@@ -124,7 +158,5 @@ public class Solution {
         } else {
             System.out.println("empty directory");
         }
-        // f.loadWordstodict(filename1);
-        // f.loadWordstodict(filename2);
     }
 }
