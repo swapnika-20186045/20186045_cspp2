2d154b83ffbf7da9f47fc03b087757fb475071b6
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-09-23 13:47:08 +0530

    "check_style_score -> 3 of 3 passed. style score: 1/1 "

--- CSPP-2 assignments/Stringmatching/temp.java
+++
@@ -1,7 +0,0 @@
-import java.util.Arrays;
-public class temp {
-	public static void main(String[] args) {
-		String t = "Prudhvik\\is\\a\\good\\boy";
-		System.out.println(Arrays.toString(t.split("good")));
-	}
-}
